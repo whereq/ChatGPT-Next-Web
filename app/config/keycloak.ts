6529f8c1dbb4bacd2bfaad4091c63f9c2ed2c61a
WhereQ <whereq@gmail.com>
2024-03-18 23:15:39 -0400

    Update png

--- app/config/keycloak.ts
+++ app/config/keycloak.ts
@@ -1,9 +1,14 @@
-export const KEYCLOAK_HOST = "http://localhost:8080";
 export const KEYCLOAK_REALM = "whereq";
 export const KEYCLOAK_CLIENT_GRANT_TYPE = "client_credentials";
 export const KEYCLOAK_CLIENT_ID = "Vegeta";
-export const KEYCLOAK_CLIENT_SECRET = "A9xx9P5lEfvEQaqQUAPAb0Am6qNSV6AA";
-export const KEYCLOAK_ISSUER = "http://localhost:8080/realms/whereq";
+
+// export const KEYCLOAK_HOST = "http://localhost:8080";
+// export const KEYCLOAK_CLIENT_SECRET = "A9xx9P5lEfvEQaqQUAPAb0Am6qNSV6AA";
+// export const KEYCLOAK_ISSUER = "http://localhost:8080/realms/whereq";
+
+export const KEYCLOAK_HOST = "https://keytomarvel.com";
+export const KEYCLOAK_CLIENT_SECRET = "7nFInd42hPl1Uz9jkepZpCm7n0OPQVCK";
+export const KEYCLOAK_ISSUER = "https://keytomarvel.com/realms/whereq";
 
 export const KEYCLOAK_CONFIG = {
   host: KEYCLOAK_HOST,
